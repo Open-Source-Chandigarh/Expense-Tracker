572449f2d4d83fc0802c2649fa18c657250b1870
Rehan Chalana <139042983+RehanChalana@users.noreply.github.com>
2023-10-22 19:05:59 +0000

    added transitions and expense Graph is working

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ let expense = document.querySelector('.expenseInput');
 let date = document.querySelector('.date');
 let hist = document.querySelector('.userEntries');
 let dummy = document.querySelector('.dummy');
-var finalExpense = '';
+var totalExpense = 0;
 let entryCount = 0;
+
+
 
 // prompting the budget
 var Balance = parseInt(prompt("SET BUDGET HERE!"));
+const BUDGET = Balance;
 while(Balance<=0){
     alert("Please Enter a Positive Balance");
     Balance = parseInt(prompt("SET BUDGET HERE!"));
 }
 document.querySelector('.blcAmount').innerText = Balance;
-document.querySelector('#budget').innerText=Balance;
+document.querySelector('#budget').innerText = Balance;
 
 // expenseHTML = JSON.parse(localStorage.getItem('history'))|| [];
 // history();
-saveCalculation();
 
 // JSON.parse(localStorage.getItem('finalExpense'))||
 
 function findInput() {
     const inputElement = document.querySelector('.expenseInput').value;
     // const expense = inputElement.value;
-    saveCalculation();
     return inputElement;
 };
 
@@ -37,25 +38,12 @@ function changeBalance() {
 
 function addExpense() {
     const newexpense = findInput();
-    const newcal = finalExpense + '+' + newexpense;
-    var answer = eval(newcal);
-    saveCalculation();
-    return answer; 
+    const newcal = totalExpense + '+' + newexpense;
+    totalExpense = eval(newcal);
+    document.querySelector('.expenseValue').innerText = totalExpense;
 };
 
-function showAnswer() {
-    finalExpense = addExpense();
-    document.querySelector('.expenseValue').innerText = finalExpense;
-    saveCalculation();
-};
 
-// function saveCalculation(){
-//     localStorage.setItem('finalExpense',finalExpense);
-//     localStorage.setItem('Balance',Balance);
-//     localStorage.setItem('expenseHTML',expenseHTML);
-// };
-
-// let expenseHTML = JSON.parse(localStorage.getItem('history'))|| [];
 function history() {
     const html = `
                 <div id="entry">
@@ -81,15 +69,26 @@ function history() {
     entry.innerHTML = html;
     hist.insertBefore(entry,hist.firstChild);
     entryCount++;
+    
+    // document.querySelector('.progress-bar').style.setProperty('backgroundImage','radial-gradient(closest-side, black 79%, transparent 80%),conic-gradient(#720e9e 10%, white 0);');
     // document.querySelector('.history').innerHTML += html;
+
     emptyInput();
-    saveCalculation();
 }
 
-function saveCalculation(){
-    localStorage.setItem('finalExpense',finalExpense);
-    localStorage.setItem('Balance',Balance);
-};
+function updateProgressBar(){
+    let expensePercentage = ((totalExpense / BUDGET ))*100;
+    expensePercentage = expensePercentage.toFixed(0);
+    if(expensePercentage<10){
+        document.querySelector("#graphPer").innerText="0"+expensePercentage+"%";
+    } else{
+        document.querySelector("#graphPer").innerText=expensePercentage+"%";
+    }
+    document.querySelector("#graphPer").innerText=expensePercentage+"%";
+    console.log(expensePercentage+" "+totalExpense+Balance);
+    document.getElementById('progress-bar').style.backgroundImage=`radial-gradient(closest-side, black 79%, transparent 80%),conic-gradient(#720e9e ${parseFloat(expensePercentage)}%, white 0)`;
+}
+
 
 function emptyInput(){
     title.focus()

86afab6bb9f1b58a62eec29ffe919f4dd4ee60f2
Kola Prabhas <kolaprabhas2310@gmail.com>
2023-10-26 22:04:21 +0530

    Added validations, so that only expense values greater than zero are accepted

--- script.js
+++ script.js
@@ -25,7 +25,13 @@ saveCalculation();
 
 function findInput() {
     const inputElement = document.querySelector('.expenseInput').value;
-    // const expense = inputElement.value;
+	// const expense = inputElement.value;
+	
+	if (inputElement <= 0) {
+		throw new Error("Expense must be greater than zero!!");
+		return;
+	}
+
     saveCalculation();
     return inputElement;
 };
@@ -36,7 +42,14 @@ function changeBalance() {
 }
 
 function addExpense() {
-    const newexpense = findInput();
+	let newexpense;
+
+	try {
+		newexpense = findInput();
+	} catch (error) {
+		alert(error.message);
+	}
+
     const newcal = finalExpense + '+' + newexpense;
     var answer = eval(newcal);
     saveCalculation();
